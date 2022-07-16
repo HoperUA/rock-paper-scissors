85895cef340e3aa3937f402480bd069ee9dff299
HoperUA <hoper.philya@gmail.com>
2022-07-16 20:11:41 +0300

    Make 'round' code and player's imput function and computerPlayer random function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+const options = ["Paper", "Rock", "Scissors"];
+function getComputerChoice() {
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+const playerSelection = prompt("Choose Your Weapon");
+const computerSelection = getComputerChoice();
+
+function playRound (playerSelection, computerSelection){
+    if (playerSelection.toLowerCase() == "Rock" && computerSelection.toLowerCase() == "Scissors"){
+        return "win";
+    }else if (playerSelection.toLowerCase() == "Paper" && computerSelection.toLowerCase() == "Rock"){
+        return "win";
+    }else if (playerSelection.toLowerCase() == "Scissors" && computerSelection.toLowerCase() == "Paper"){
+        return "win";
+    }else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
+        return "tie";
+    }else {
+        return "loss";
+    }
+}
+console.log(playerSelection.toLowerCase());
+console.log(computerSelection.toLowerCase());
+console.log(playRound(playerSelection, computerSelection));
+
+//I need to mak the Game function with the loop of 5 rounds
