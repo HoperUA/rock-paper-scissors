a217a7694a28b05a77f5f1f1502f5fa8227fbd09
HoperUA <hoper.philya@gmail.com>
2022-07-17 15:02:45 +0300

    Make rework of playRound function

--- script/script.js
+++ script/script.js
@@ -1,26 +1,54 @@
-const options = ["Paper", "Rock", "Scissors"];
+let playerSelection;
+let computerSelection;
+const playerScore = 0;
+const computerScore = 0;
+
+
+const options = ["paper", "rock", "scissors"];
 function getComputerChoice() {
     return options[Math.floor(Math.random() * options.length)];
 }
 
-const playerSelection = prompt("Choose Your Weapon");
-const computerSelection = getComputerChoice();
 
-function playRound (playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() == "Rock" && computerSelection.toLowerCase() == "Scissors"){
-        return "win";
-    }else if (playerSelection.toLowerCase() == "Paper" && computerSelection.toLowerCase() == "Rock"){
-        return "win";
-    }else if (playerSelection.toLowerCase() == "Scissors" && computerSelection.toLowerCase() == "Paper"){
-        return "win";
-    }else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-        return "tie";
-    }else {
-        return "loss";
-    }
+const playRound = function(playerSelection, computerSelection) {
+    playerSelection = prompt("Choose Your Weapon");
+    computerSelection = getComputerChoice();
+    console.log(playerSelection.toLowerCase());
+    console.log(computerSelection);
+    playerSelection = playerSelection.toLowerCase();
+
+        if ((playerSelection === "rock" && computerSelection === "scissors") || 
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")){
+            console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+            return "win";
+
+        }else if ((playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors")){
+            console.log(`You lost, ${computerSelection} beats ${playerSelection}!`);
+            return "win";
+
+        }else if (playerSelection == computerSelection){
+            console.log(`You both choose ${computerSelection}, it's a tie!`);
+            return "tie";
+
+        }else {
+            console.log("Error, try again");
+            return playRound();
+        }   
 }
-console.log(playerSelection.toLowerCase());
-console.log(computerSelection.toLowerCase());
-console.log(playRound(playerSelection, computerSelection));
 
-//I need to mak the Game function with the loop of 5 rounds
+
+
+
+
+
+
+
+
+
+
+//console.log(playerSelection.toLowerCase());
+//I need to make the Game function with the loop of 5 rounds
+
